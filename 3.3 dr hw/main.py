0e31ef46597ffa107c93b522600e5f956a336d25
Insomnia01001 <167763964+Insomnia01001@users.noreply.github.com>
2024-10-22 19:59:20 +0300

    Add files via upload

---
+++ 3.3 dr hw/main.py
@@ -0,0 +1,40 @@
+# def javob_func(*args):
+#     a= min(args)
+#     b= max(args)
+#     return a,b
+# result = javob_func(1,2,3,3,4,67,89,33,45,200,180)
+# print(result)
+
+    
+# def func1(*args):
+#     return " ".join(args)
+# result1 = func1("My","Name","Is","Alex")
+# print(result1)
+
+# def func2(*args):
+#     result =[]
+#     for i in args:
+#         result.append(len(i))
+#     return result
+# result2 = func2("TExt ","absudab","HELLO WORLD")
+# print(result2)
+
+
+
+# def func3 (*args):
+#     for i in range(len(args)):
+#         print(len(args[i]))
+# result3 = func3("salom","world","ajbdduidaoid","adniaadjmoiadmoaidmad")
+# print(result3,end=" ")
+
+
+
+# 4
+# def kw(**kwargs):
+#     for key,value in kwargs.items():
+#         print(f"{key} {value}")
+# result =kw(name="Ali",age="25")
+# print(result)
+
+
+
